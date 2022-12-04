c9beed0acf8b3fe0cd19dc8b161da15fb302ddcb
peernisse <peernisse@utah.gov>
2022-12-04 06:37:59 -0700

    Initial commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,12 +20,12 @@
 ##
 golem::fill_desc(
   pkg_name = "riverPlanner", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
-  repo_url = NULL # The URL of the GitHub Repo (optional)
+  pkg_title = "River Trip Planner", # The Title of the package containing the App
+  pkg_description = "Modular application to plan gear, crew contacts, and menu.", # The Description of the package containing the App
+  author_first_name = "Peter", # Your First Name
+  author_last_name = "EerNisse", # Your Last Name
+  author_email = "peernisse@gmail.com", # Your Email
+  repo_url = "https://github.com/peernisse/riverPlanner" # The URL of the GitHub Repo (optional)
 )
 
 ## Set {golem} options ----
